2f42c845973764fb2113d41104849be40f6b7c3a
suncmm <915906000@qq.com>
2022-06-16 19:40:10 +0800

    Format global variable

--- newgame.py
+++ newgame.py
@@ -1,10 +1,23 @@
+from http.client import ImproperConnectionState
 import pygame
+from pygame.locals import(
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
 
 
 
 
 def main():
+    #  Initialize pygame
     pygame.init()
+    SCREEN_WIDTH = 1200
+    SCREEN_HEIGHT = 700
     pygame.display.set_caption('Revolution of Godding Human - version1.0')
     win = pygame.display.set_mode((1200, 700))
     running = True
